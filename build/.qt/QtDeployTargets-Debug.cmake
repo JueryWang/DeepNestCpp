5bf34e8ec05af98f6497ce7a5b3e2235adfc0ccb
JueryWang <TaylorWalters1971566@gmail.com>
2025-07-09 14:37:01 +0800

    delete directory build

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DeepNestCpp_FILE E:/DeepNestCpp/build/Debug/DeepNestCpp.exe)
-set(__QT_DEPLOY_TARGET_DeepNestCpp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DeepNestCpp_RUNTIME_DLLS E:/Qt/6.7.3/msvc2019_64/bin/Qt6Widgetsd.dll;E:/Qt/6.7.3/msvc2019_64/bin/Qt6Guid.dll;E:/Qt/6.7.3/msvc2019_64/bin/Qt6Cored.dll)
--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DeepNestCpp_FILE E:/DeepNestCpp/build/MinSizeRel/DeepNestCpp.exe)
-set(__QT_DEPLOY_TARGET_DeepNestCpp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DeepNestCpp_RUNTIME_DLLS E:/Qt/6.7.3/msvc2019_64/bin/Qt6Widgets.dll;E:/Qt/6.7.3/msvc2019_64/bin/Qt6Gui.dll;E:/Qt/6.7.3/msvc2019_64/bin/Qt6Core.dll)
